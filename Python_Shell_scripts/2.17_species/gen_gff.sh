e7fa34ee328faf5a32393bcdd36fa181134636ac
tli14 <57804794+tli14@users.noreply.github.com>
2021-11-24 16:43:50 -0600

    Rename .gff files

---
+++ Python_Shell_scripts/2.17_species/gen_gff.sh
@@ -0,0 +1,11 @@
+# 2. Rename .gff files from Prokka results.
+
+## gen_gff.sh
+
+cat << EOF> gff.sh
+#!/bin/sh
+while read gff_list
+do
+        mv \$gff_list "\$( dirname "\$gff_list" ).gff"
+done <\$1
+EOF

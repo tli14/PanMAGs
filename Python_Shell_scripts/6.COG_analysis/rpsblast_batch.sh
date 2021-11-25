bc13511070a6ad3500d5df97660f140e5e54848a
tli14 <57804794+tli14@users.noreply.github.com>
2021-11-24 20:06:30 -0600

    Generate rps-blast script

---
+++ Python_Shell_scripts/6.COG_analysis/rpsblast_batch.sh
@@ -0,0 +1,10 @@
+## 5. Use rps-blast to perform domian search and then determine COG categories. (blast/2.10)
+
+# rpsblast_batch.sh
+input=$1
+output=$1_rpsblast_output
+
+cat << EOF> rpsblast.sh
+#!/bin/sh
+rpsblast -query $input -db COG/COG_CDD/Cog -outfmt 6 -evalue 1E-5 -out $output -num_threads 32
+EOF>
